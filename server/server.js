20558041428a20d1305f13567f44d84fa7a31e46
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-14 12:55:14 +0430

    packages installed

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("course api");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
